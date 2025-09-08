f2921f570afe98f7fc3caf0f1187f8ad1c6b429c
Yihang Peng <ypeng0082@gmail.com>
2025-09-07 22:18:55 -0400

    update

--- set_alias.sh
+++ set_alias.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cat << 'EOF' >> ~/.bashrc
+cat >> ~/.bashrc << 'EOF'
 alias sq="squeue -u your_netid --format='%.18i %.9P %.14j %.2t %.10M %.6D %.4C %R'"
 alias sqd="squeue -u your_netid --format='%.18i %.9P %.8j %.2t %.10M %.6D %Z'"
-
 EOF
